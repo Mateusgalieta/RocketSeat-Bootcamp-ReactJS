a074efc44aabbf7d4e581374a5e9d64214ff095e
Mateus Galieta <mateus.galieta@aquino.ind.br>
2020-04-17 10:08:30 -0300

    State and Imutability

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header';
 
 function App() {
+
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    // useState retorna um array com 2 posicoes
+
+    function handleAddProject() {
+        // projects.push(`Novo projeto ${Date.now()}`);
+
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    }
+
     return (
         <div>
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
             <Header title="Projects" />
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li> )}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </div>
     );
 }
