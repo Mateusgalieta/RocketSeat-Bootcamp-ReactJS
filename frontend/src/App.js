e8043afe70f586ed276e7acf57f9927f15224d57
Mateus Galieta <mateus.galieta@aquino.ind.br>
2020-04-20 09:42:12 -0300

    list api projects

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import Header from './components/Header';
-
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 function App() {
 
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
 
     // useState retorna um array com 2 posicoes
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -19,10 +23,8 @@ function App() {
         <div>
             <Header title="Projects" />
 
-            <img width={300} src={backgroundImage} alt=""/>
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li> )}
+                {projects.map(project => <li key={project.id}>{project.title}</li> )}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
